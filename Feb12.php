259cafe7c2390358c5f63da70aec1196d2f36678
beloved-git <beloved_andrei@qq.com>
2026-02-18 15:25:25 +0800

    Add files via upload
    first

---
+++ Feb12.php
@@ -0,0 +1,42 @@
+博士第一学历 常委 雍和宫 载人航天器
+民航
+邮轮 经济法 宪法 项目申报 聋哑英语
+
+槟郎 产业 面试/补课 潜艇兵 西南财经大学 
+
+vol ex+vol=evolve volume体积 revolve revolution把社会卷起来->革命 convoluted复杂的，一团乱麻
+rule rulerl ruling class 统治阶级
+familiar family
+thousand 1,000
+million  1,000,000
+billion  1,000,000,000
+out of reach(china)
+
+remember me        re+mem+ber自己想起  re+mind被人让你想起：提醒
+though I have to say goodbye. 尽管  我不得不
+Don't let it make you cry. For(because) even即便 I'm far away,I hold you in my heart.
+I sing a secret秘密的 song to you each night we apart. 4->wonderful ea
+Each student has a different dream.     每个人的梦不一样  each other 个个不同
+Every(All) student must wear a uniform.  整体>个人                  全都不落
+Though I have to travel far，远行   travel，journey本意并不好，现在中立了
+Each time you hear a sad guitar，know (that) = (I'm with you). guitar 来自于西班牙语🇪🇸
+The only {way <-（that I can be）} / until you're in my 怀抱arms again.  arm武器
+
+take part参加，想象拿走一块🍰，参加了吃蛋糕 apart
+
+Prism Allay Comet🌠 
+stable table st /sd/    sta-
+accept receive perceive  ceive 抓拿
+co-ordin（‘order’顺序，秩序）+ate 标准化的定位系统。坐标
+re+ad+just 再次 让它（朝向，去） 刚刚好 再次调整，重置      ad+just 让它+刚刚好->调整
+stand by 站在身旁->待命中
+po+si+tion
+eject ex+ject  evolve ex+vol   reject拒绝,扔回来re+ject  ject扔，投掷
+
+Everything is made(make,被动分词) to fit.
+trade war贸易战
+smooth 平稳的，光滑的  oo (常见)呜呜 moon/mushroom/boom/doom/tool/school  呜 good/wood/foot
+door，floor，poor（oor,oar,our）soar,course
+
+
+1+2+3+....+98+99+100   101,101,..,...nn==50 101*50=5050
